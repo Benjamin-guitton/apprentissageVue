2102a10d3be56ed6fcb6036af727f23cd5652f81
Benjamin-guitton <benjamin.guittons@outlook.com>
2022-01-26 15:39:40 +0100

    Ajout fin projet vue ideas (Ne marche qu'avec Vue2)

--- vuex-ideas/src/components/Ideas.vue
+++ vuex-ideas/src/components/Ideas.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
-    <h1>Ideas</h1>
-    <ul>
-      <draggable v-model="allIdeas" item-key="id">
-      <template #item="{element}">
-        <idea {{element.title}} />
-      </template>
-     <!-- <ul>
-        <li v-for="idea in allIdeas" :key="idea.id">{{ idea.title }}</li>
+    <div>
+      <h1>Ideas</h1>
+      <ul>
+        <draggable class="ideas" v-model="allIdeas" group="ideas">
+          <Idea v-for="idea in AllIdeas" :key="idea.id" :item="idea"/>
+        </draggable>
+      </ul>
+      <!-- <ul>
+        <draggable v-model="allIdeas" item-key="id">
+          <template #item="{element}">
+            <idea {{element.title}} />
+          </template>
+        </draggable>
       </ul> -->
-      </draggable>
-    </ul>
+    </div>
+    <div class="destination">
+      <div>
+        <h3>Courses</h3>
+        <draggable class="ideas" v-model="courses" group="ideas">
+          <Idea v-for="course in courses" :key="course.id" :="course"/>
+        </draggable>
+      </div>
+    </div>
+    <h3>Paper work</h3>
+    <draggable class="ideas" v-model="courses" group="ideas">
+      <Idea v-for="pw in paperWork" :key="pw.id" :item="pw" />
+    </draggable>
   </div>
-
 </template>
 
 <script>
@@ -25,8 +40,29 @@ export default {
     Idea,
   },
   computed: {
-    allIdeas() {
-      return this.$store.getters.allIdeas;
+    allIdeas: {
+      get() {
+        return this.$store.getters.allIdeas;
+      },
+      set(value) {
+        this.$store.dispatch("saveIdeasOrdering", value);
+      }
+    },
+    courses: {
+      get() {
+        return this.$store.getters.courses
+      },
+      set(value) {
+        this.$store.dispatch("saveCoursesOrdering", value);
+      }
+    },
+    paperWork: {
+      get() {
+        return this.$store.getters.paperWork;
+      },
+      set(value) {
+        this.$store.dispatch("savePaperWorkOrdering", value);
+      }
     }
   },
   name: "Ideas"
@@ -34,5 +70,15 @@ export default {
 </script>
 
 <style scoped>
+.ideas {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+}
+.destination {
+  display: flex;
+  justify-content: space-evenly;
+}
 
 </style>
--- vuex-ideas/src/store/index.js
+++ vuex-ideas/src/store/index.js
@@ -2,28 +2,61 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    ideas: {}     // Si ideas valeur = []
+    ideas: {},     // Si ideas valeur = []
+    courses: [],
+    paperWork: []
   },
   mutations: {
     CREATE_IDEAS(state, payload){
       const id = Date.now();
       state.ideas[id] = payload;
       state.ideas = {...state.ideas};  // Alors remplacer {} par []
+    },
+    SAVE_IDEAS_ORDERING(state, payload) {
+      state.ideas = { ...payload };
+    },
+    SAVE_COURSES_ORDERING(state, payload) {
+      state.courses = [...payload ];
+    },
+    SAVE_PAPERWORK_ORDERING(state, payload) {
+      state.paperWork = [ ...payload ];
     }
   },
   actions: {
     createIdea({ commit }, payload) {
       console.log("action / createIdea")
       commit('CREATE_IDEAS', payload);
+    },
+    saveIdeasOrdering({ commit }, payload) {
+      const ideas = {};
+      payload.map(idea => {
+        ideas[idea.id] = idea;
+      });
+      commit("SAVE_IDEAS_ORDERING", ideas);
+    },
+    saveCoursesOrdering({ commit }, payload) {
+      commit("SAVE_COURSES_ORDERING", payload);
+    },
+    savePaperWorkOrdering({ commit }, payload) {
+      commit("SAVE_PAPERWORK_ORDERING", payload);
     }
   },
   getters: {
     allIdeas(state) {
-      const allIdeas = Object.keys(state.ideas).map(key => state.ideas[key]);
+      const allIdeas = Object.keys(state.ideas).map(key => {
+        const idea = state.ideas[key];
+        idea.id = key;
+        return idea;
+      });
       console.log("getters", allIdeas);
       return allIdeas;
+    },
+    courses(state) {
+      return state.courses;
+    },
+    paperWork(state) {
+      return state.paperWork;
     }
-    // { 123: {}, 345: {}} => [{}, {}, {}]
   },
   modules: {},
 });
